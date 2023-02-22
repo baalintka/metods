b29b30945823ae2c562e9bb7325651088b7f20f7
baalintka <ballint3@gmail.com>
2023-02-22 14:54:21 +0100

    metodusok fgyakorlasa

--- src/metodusok/Metodusok.java
+++ src/metodusok/Metodusok.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package metodusok;
 
-/**
- *
- * @author balli
- */
+
 public class Metodusok {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+    }
+    
+    private static int elso10szamOsszege() {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg+=i;
+        }
+        return osszeg;
+        
+    }
+    
+        private static int osszead(int a, int b){
+            return a+b;
     }
     
 }
+   
+
