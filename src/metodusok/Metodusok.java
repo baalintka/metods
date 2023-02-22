8e10e109ff389d593ed20c6d79b4fbbab9e495f7
baalintka <ballint3@gmail.com>
2023-02-22 15:05:19 +0100

    nemtalalom  a hibát

--- src/metodusok/Metodusok.java
+++ src/metodusok/Metodusok.java
@@ -6,7 +6,17 @@ public class Metodusok {
 
     
     public static void main(String[] args) {
-       
+        
+        int osszeg=elso10szamOsszege();
+        String kimenet = "Az első 10 szám összege:" + osszeg + "\n";
+        kiír(kimenet);
+        
+        int szam1=7, szam2 = 4;
+        osszeg = osszead(szam1, szam2);
+        kiír("%d + %d = %d\n".formatted(szam1,szam2,osszeg));
+        
+        
+        
     }
     
     private static int elso10szamOsszege() {
@@ -20,6 +30,10 @@ public class Metodusok {
     
         private static int osszead(int a, int b){
             return a+b;
+    }
+
+    private static void kiír(String kimenet) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
