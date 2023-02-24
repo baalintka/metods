28898e0eb1989a81610e4734ab84f787c6bb123a
baalintka <ballint3@gmail.com>
2023-02-24 12:02:14 +0100

    equlizer metods

--- src/oraimunka/equlizer.java
+++ src/oraimunka/equlizer.java
@@ -1,11 +1,37 @@
 package oraimunka;
 
+import java.util.Random;
+
 
 public class equlizer {
     
+    private static Random rnd = new Random();
+    
    public static void main(String[]args){
        
+       equlizer1();
        
        
+
    }
+
+    private static void equlizer1() {
+        eq();
+        eq();
+        eq();
+        eq();
+        eq();
+        
+    }
+    private static void eq(){
+        int db = rnd.nextInt(3,8);
+        String szin= "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.println(szin+"");
+        }
+        System.out.println("");
+    }
+    
+    
+    
 }
