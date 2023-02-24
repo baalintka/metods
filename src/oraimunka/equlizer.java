81f5788275e85194eecfde16e47c8e8f5607213d
baalintka <ballint3@gmail.com>
2023-02-24 12:03:04 +0100

    equlizer metodss

--- src/oraimunka/equlizer.java
+++ src/oraimunka/equlizer.java
@@ -7,31 +7,38 @@ public class equlizer {
     
     private static Random rnd = new Random();
     
-   public static void main(String[]args){
-       
-       equlizer1();
-       
-       
+        public static void main(String[]args){
 
-   }
+            equlizer1();
 
-    private static void equlizer1() {
-        eq();
-        eq();
-        eq();
-        eq();
-        eq();
-        
-    }
-    private static void eq(){
-        int db = rnd.nextInt(3,8);
-        String szin= "\u001B[45m";
-        for (int i = 0; i < db; i++) {
-            System.out.println(szin+"");
+
+
         }
-        System.out.println("");
-    }
-    
-    
-    
-}
+
+         private static void equlizer1() {
+             eq();
+             eq(12);
+             eq();
+             eq(8);
+             eq();
+
+         }
+         private static void eq(){
+             int db = rnd.nextInt(8);
+             String szin= "\u001B[45m";
+             for (int i = 0; i < db; i++) {
+                 System.out.println(szin+"");
+             }
+             System.out.println("");
+         }
+
+         private static void eq(int hossz){
+             
+             hossz=12;
+             String szin= "\u001B[45m";
+             for (int i = 0; i < hossz; i++) {
+                 System.out.println(szin+"");
+             }
+         }
+
+     }
