21c09971605ad8a24572c6c3ffce09a91b9cb00d
baalintka <ballint3@gmail.com>
2023-02-24 12:20:34 +0100

    equlizer booleasn

--- src/oraimunka/equlizer.java
+++ src/oraimunka/equlizer.java
@@ -2,47 +2,39 @@ package oraimunka;
 
 import java.util.Random;
 
-
 public class equlizer {
-    
+
     private static Random rnd = new Random();
-    
-        public static void main(String[]args){
 
-            equlizer1();
+    public static void main(String[] args) {
 
+        equlizer1();
 
+    }
 
+    private static void equlizer1() {
+        eq();
+        eq(8);
+        eq();
+        eq(12);
+        eq();
+
+    }
+
+    private static void eq() {
+        int db = rnd.nextInt(8);
+        eq(db);
+    }
+
+    private static void eq(int hossz) {
+
+        String csillag = "*";
+        for (int i = 0; i < hossz; i++) {
+            System.out.println(csillag + "");
         }
-
-         private static void equlizer1() {
-             eq();
-             eq();
-             eq();
-             eq();
-             eq();
-
-         }
-         private static void eq(){
-             int db = rnd.nextInt(8);
-             eq(db);
-         }
-
-         private static void eq(int hossz){
-             
-            
-             String csillag= "*";
-             for (int i = 0; i < hossz; i++) {
-                 System.out.println(csillag+"");
-             }
-             System.out.println("");
+        System.out.println("");
           
-         private static void eq(boolean igaz){
-             igaz=true;
-             if igaz {
-             
-             
-         }
-         }
-
-     
+         
+    }
+    
+    }
